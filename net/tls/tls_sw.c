ecd34116dc93c459891a310fd25ed9f066923cd2
Vakul Garg <vakul.garg@nxp.com>
2019-02-11 11:31:05 +0000

    UPSTREAM: net/tls: Do not use async crypto for non-data records
    Addition of tls1.3 support broke tls1.2 handshake when async crypto
    accelerator is used. This is because the record type for non-data
    records is not propagated to user application. Also when async
    decryption happens, the decryption does not stop when two different
    types of records get dequeued and submitted for decryption. To address
    it, we decrypt tls1.2 non-data records in synchronous way. We check
    whether the record we just processed has same type as the previous one
    before checking for async condition and jumping to dequeue next record.
    
    Fixes: 130b392c6cd6b ("net: tls: Add tls 1.3 support")
    Change-Id: I11af5e8ac20eaf47c7564759f86c642fbc9b58c6
    Signed-off-by: Vakul Garg <vakul.garg@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -1591,10 +1591,10 @@ int tls_sw_recvmsg(struct sock *sk,
 
 	while (len && (decrypted + copied < target || ctx->recv_pkt)) {
 		bool retain_skb = false;
-		bool async = false;
 		bool zc = false;
 		int to_decrypt;
 		int chunk = 0;
+		bool async;
 
 		skb = tls_wait_data(sk, psock, flags, timeo, &err);
 		if (!skb) {
@@ -1619,18 +1619,21 @@ int tls_sw_recvmsg(struct sock *sk,
 		    ctx->control == TLS_RECORD_TYPE_DATA)
 			zc = true;
 
+		/* Do not use async mode if record is non-data */
+		if (ctx->control == TLS_RECORD_TYPE_DATA)
+			async = ctx->async_capable;
+		else
+			async = false;
+
 		err = decrypt_skb_update(sk, skb, &msg->msg_iter,
-					 &chunk, &zc, ctx->async_capable);
+					 &chunk, &zc, async);
 		if (err < 0 && err != -EINPROGRESS) {
 			tls_err_abort(sk, EBADMSG);
 			goto recv_end;
 		}
 
-		if (err == -EINPROGRESS) {
-			async = true;
+		if (err == -EINPROGRESS)
 			num_async++;
-			goto pick_next_record;
-		}
 
 		if (!cmsg) {
 			int cerr;
@@ -1648,6 +1651,9 @@ int tls_sw_recvmsg(struct sock *sk,
 		} else if (control != ctx->control) {
 			goto recv_end;
 		}
+
+		if (async)
+			goto pick_next_record;
 
 		if (!zc) {
 			if (rxm->full_len > len) {
